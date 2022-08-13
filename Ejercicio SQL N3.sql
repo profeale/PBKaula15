298fd12c9c1e4c3584118b2fb401f3d9d3b151c5
verodam <verodambrosio2@gmail.com>
2022-08-13 18:40:55 -0300

    Ejercicio sql 3

--- Ejercicio SQL N3.sql
+++ Ejercicio SQL N3.sql
@@ -1,3 +1,3 @@
 delete from Perro where id_perro in (
-select id_perro from Historial where EXTRACT(YEAR FROM fecha) < 2010
+select distinct perro from Historial where EXTRACT(YEAR FROM fecha) < 2010
 )
